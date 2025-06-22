f0fca36d2bf7ad126ec33d9eecf9b6a7a4a0b388
CarlosMendes97 <carlos.danrley1997@gmail.com>
2025-06-22 17:07:53 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,90 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM 10
+
+void inicializarTabuleiro(char tabuleiro[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            tabuleiro[i][j] = '0';
+}
+
+void posicionarNavio(int navio[3][2]) {
+    int orientacao = rand() % 2; // 0 = horizontal, 1 = vertical
+    int linha, coluna;
+
+    if (orientacao == 0) {
+        linha = rand() % TAM;
+        coluna = rand() % (TAM - 2);
+        for (int i = 0; i < 3; i++) {
+            navio[i][0] = linha;
+            navio[i][1] = coluna + i;
+        }
+    } else {
+        linha = rand() % (TAM - 2);
+        coluna = rand() % TAM;
+        for (int i = 0; i < 3; i++) {
+            navio[i][0] = linha + i;
+            navio[i][1] = coluna;
+        }
+    }
+}
+
+int verificarAcerto(int navio[3][2], int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (navio[i][0] == linha && navio[i][1] == coluna)
+            return 1;
+    }
+    return 0;
+}
+
+void mostrarTabuleiro(char tabuleiro[TAM][TAM]) {
+    printf("\n   ");
+    for (int i = 0; i < TAM; i++) printf("%2d", i);
+    printf("\n");
+    for (int i = 0; i < TAM; i++) {
+        printf("%2d ", i);
+        for (int j = 0; j < TAM; j++) {
+            printf("%2c", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char tabuleiro[TAM][TAM];
+    int navio[3][2], linha, coluna, acertos = 0, tentativas = 0;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    srand(time(NULL));
+    inicializarTabuleiro(tabuleiro);
+    posicionarNavio(navio);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("=== BATALHA NAVAL 10x10 ===\n");
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    while (acertos < 3) {
+        mostrarTabuleiro(tabuleiro);
+        printf("Digite linha e coluna (0 a %d): ", TAM - 1);
+        scanf("%d%d", &linha, &coluna);
+        tentativas++;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        if (tabuleiro[linha][coluna] == '*' || tabuleiro[linha][coluna] == '3') {
+            printf(">> Já tentou aqui!\n");
+            continue;
+        }
+
+        if (verificarAcerto(navio, linha, coluna)) {
+            printf(">> BOOM! Acertou parte do navio!\n");
+            tabuleiro[linha][coluna] = '3';
+            acertos++;
+        } else {
+            printf(">> Água!\n");
+            tabuleiro[linha][coluna] = '*';
+        }
+    }
+
+    mostrarTabuleiro(tabuleiro);
+    printf("Parabéns! Você afundou o navio em %d tentativas!\n", tentativas);
 
     return 0;
 }
